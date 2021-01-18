a517f98637b910e643d8356553b9485ca3b2a43c
Baba Dorin <bvd.dorin@gmail.com>
2021-01-18 10:53:12 +0200

    bb2

---
+++ BugBounty/BugBounty/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+using System;
+using System.Drawing;
+
+namespace BugBounty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool c1 = (textBox1.TabIndex < textBox2.TabIndex && textBox2.TabIndex < button1.TabIndex);
+            bool c2 = (button1.Parent == this && button1.Dock == System.Windows.Forms.DockStyle.Bottom);
+
+            if (c1 && c2)
+            {
+                this.BackgroundImage = new Bitmap(BugBounty.Properties.Resources.ric);
+                panel1.BackColor = Color.Transparent;
+            }
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(298, 296);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Aici va aparea numarul aleator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pana la:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "De la:";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Extrage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(300, 296);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private object tbDela;
+    }
+}
+
---
+++ BugBounty/BugBounty/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugBounty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Problema 1>
+            // Doresc sa ma deplasez printre textbox-uri utilizand tasta tab.
+            // La moment, daca selectez cu mouse-ul textbox-ul din stanga, la apasarea tastei TAB
+            // este focusat butonul de extragere, iar mai apoi - textbox-ul din dreapta.
+
+            // Doresc ca TAB-ul sa ma directioneze de la textbox-ul din stanga catre textbox-ul din dreapta, iar mai apoi - 
+            // catre buton
+
+
+            // Problema 2>
+            // Doresc ca butonul de extragere sa fie mereu lipit de margina de jos a ferestrei.
+            // Pentru a face acest lucru am setat proprietatea Dock cu valoarea Buttom.
+            // Totusi, butonul nu este lipit de margina de jos a ferestrei, puteti sa va convingeti de acest lucru
+            // prin a da la executie programul si a redimensiona fereastra. Veti vedea ca butonul sta chill
+            // acolo unde are el chef si nu se deplaseaza dupa margina de jos.
+
+            // Ajuta-ma sa andochez de marginea de jos a ferestrei butonul de extragere ;)
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int min) && int.TryParse(textBox2.Text, out int max))
+            {
+                try
+                {
+                    label3.Text = RandomNumberBetween(min, max).ToString();
+                }
+                catch (Exception)
+                {
+                    label3.Text = "???";
+                }
+            }
+        }
+
+        private int RandomNumberBetween(int minValue, int maxValue) => (new Random()).Next(minValue, maxValue);
+    }
+}
