0628ca71ca1f137f39aecb45fd4cca9b8ff3ed3d
Baba Dorin <bvd.dorin@gmail.com>
2021-04-05 10:34:53 +0300

    Convertor valutar added

---
+++ Lectia 12/Sarcina2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Sarcina2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInitial = new System.Windows.Forms.TextBox();
+            this.tbFinal = new System.Windows.Forms.TextBox();
+            this.cmbInitial = new System.Windows.Forms.ComboBox();
+            this.cmbFinal = new System.Windows.Forms.ComboBox();
+            this.btConvertire = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInitial
+            // 
+            this.tbInitial.Location = new System.Drawing.Point(32, 57);
+            this.tbInitial.Name = "tbInitial";
+            this.tbInitial.Size = new System.Drawing.Size(130, 29);
+            this.tbInitial.TabIndex = 0;
+            // 
+            // tbFinal
+            // 
+            this.tbFinal.Enabled = false;
+            this.tbFinal.Location = new System.Drawing.Point(214, 57);
+            this.tbFinal.Name = "tbFinal";
+            this.tbFinal.Size = new System.Drawing.Size(130, 29);
+            this.tbFinal.TabIndex = 1;
+            // 
+            // cmbInitial
+            // 
+            this.cmbInitial.FormattingEnabled = true;
+            this.cmbInitial.Location = new System.Drawing.Point(32, 92);
+            this.cmbInitial.Name = "cmbInitial";
+            this.cmbInitial.Size = new System.Drawing.Size(130, 32);
+            this.cmbInitial.TabIndex = 2;
+            // 
+            // cmbFinal
+            // 
+            this.cmbFinal.FormattingEnabled = true;
+            this.cmbFinal.Location = new System.Drawing.Point(214, 92);
+            this.cmbFinal.Name = "cmbFinal";
+            this.cmbFinal.Size = new System.Drawing.Size(130, 32);
+            this.cmbFinal.TabIndex = 3;
+            // 
+            // btConvertire
+            // 
+            this.btConvertire.Location = new System.Drawing.Point(32, 130);
+            this.btConvertire.Name = "btConvertire";
+            this.btConvertire.Size = new System.Drawing.Size(312, 34);
+            this.btConvertire.TabIndex = 4;
+            this.btConvertire.Text = "Converteste";
+            this.btConvertire.UseVisualStyleBackColor = true;
+            this.btConvertire.Click += new System.EventHandler(this.btConvertire_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 221);
+            this.Controls.Add(this.btConvertire);
+            this.Controls.Add(this.cmbFinal);
+            this.Controls.Add(this.cmbInitial);
+            this.Controls.Add(this.tbFinal);
+            this.Controls.Add(this.tbInitial);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInitial;
+        private System.Windows.Forms.TextBox tbFinal;
+        private System.Windows.Forms.ComboBox cmbInitial;
+        private System.Windows.Forms.ComboBox cmbFinal;
+        private System.Windows.Forms.Button btConvertire;
+    }
+}
+
---
+++ Lectia 12/Sarcina2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sarcina2
+{
+    public partial class Form1 : Form
+    {
+        double[,] RateleDeConversie = new double[4,4];
+
+        readonly int MDL = 0;
+        readonly int EUR = 1;
+        readonly int USD = 2;
+        readonly int RON = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            RateleDeConversie[MDL, MDL] = 1;
+            RateleDeConversie[MDL, EUR] = 0.05;
+            RateleDeConversie[MDL, USD] = 0.06;
+            RateleDeConversie[MDL, RON] = 0.23;
+
+            RateleDeConversie[EUR, MDL] = 20;
+            RateleDeConversie[EUR, EUR] = 1;
+            RateleDeConversie[EUR, USD] = 1.18;
+            RateleDeConversie[EUR, RON] = 4.91;
+
+            RateleDeConversie[USD, MDL] = 18.07;
+            RateleDeConversie[USD, EUR] = 0.85;
+            RateleDeConversie[USD, USD] = 1;
+            RateleDeConversie[USD, RON] = 4.17;
+
+            RateleDeConversie[RON, MDL] = 4.33;
+            RateleDeConversie[RON, EUR] = 0.2;
+            RateleDeConversie[RON, USD] = 0.24;
+            RateleDeConversie[RON, RON] = 1;
+
+            List<object> valute1 = new List<object>() { "MDL", "EUR", "USD", "RON" };
+            List<object> valute2 = new List<object>() { "MDL", "EUR", "USD", "RON" };
+            cmbInitial.DataSource = valute1;
+            cmbFinal.DataSource = valute2;
+        }
+
+        private void btConvertire_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Extragem valoarea
+                // 2. Extragem valuta initiala
+                // 3. Extragem valuta finala
+                // 4. Afisam rezultatul final
+
+                // 1.
+                double valoareInitiala = double.Parse(tbInitial.Text);
+
+                // 2.
+                int valutaInitiala = cmbInitial.SelectedIndex;
+
+                // 3. 
+                int valutaFinala = cmbFinal.SelectedIndex;
+
+                // 4.
+                tbFinal.Text = (valoareInitiala * RateleDeConversie[valutaInitiala, valutaFinala]).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Asigurati-va ca ati introdus valori valide.");
+            }
+        }
+    }
+}
